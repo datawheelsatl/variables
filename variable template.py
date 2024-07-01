7d2231eb9d7d32e04c83921e522265d4807a4159
datawheelsatl <173591532+datawheelsatl@users.noreply.github.com>
2024-07-01 06:18:23 -0400

    Initial commit

---
+++ variable template.py
@@ -0,0 +1,10 @@
+
+character_name = "Tim"
+character_age = "43"
+is_Male = True
+print("My name is " + character_name + ", ")
+print("and I am " + character_age + " years old. ")
+
+character_name = "@Datawheelsatl"
+print("Search for my data analytic and coding work under  " + character_name + ",")
+print("I'll have my Google Dat Analytics Certificate at the young tender age of " + character_age + "")
